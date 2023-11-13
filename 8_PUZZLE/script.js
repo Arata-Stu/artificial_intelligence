11094cc337d4b10538eadbcbf0d58afd060f4436
ArataTanaka <a3033094@edu.gifu-u.ac.jp>
2023-11-13 18:41:41 +0900

    complete "DFS" and "BFS"

--- 8_PUZZLE/script.js
+++ 8_PUZZLE/script.js
@@ -1,9 +1,126 @@
+class PuzzleState {
+    constructor(tiles, parent = null, move = null) {
+        this.tiles = tiles;
+        this.parent = parent; // この状態に至る前の状態
+        this.move = move; // この状態に至るために行った移動
+    }
+
+    // 状態がゴールかどうか判定するメソッド
+    isGoal() {
+        const goalState = ['tile_0_0', 'tile_0_1', 'tile_0_2', 'tile_1_0', 'tile_1_1', 'tile_1_2', 'tile_2_0', 'tile_2_1', 'empty'];
+        return this.tiles.join('') === goalState.join('');
+    }
+
+    static dfs(startState) {
+        let stack = [startState];
+        let visited = new Set();
+
+        while (stack.length > 0) {
+            let currentState = stack.pop();
+
+            if (currentState.isGoal()) {
+                return currentState;
+            }
+
+            let stateStr = currentState.toString();
+            if (visited.has(stateStr)) {
+                continue;
+            }
+            visited.add(stateStr);
+
+            let childStates = currentState.generateChildStates();
+            childStates.forEach(state => {
+                if (!visited.has(state.toString())) {
+                    stack.push(state);
+                }
+            });
+        }
+
+        return null;
+    }
+
+    // BFSを行う静的メソッド
+    static bfs(startState) {
+        let queue = [startState];
+        let visited = new Set();
+
+        while (queue.length > 0) {
+            let currentState = queue.shift();
+
+            if (currentState.isGoal()) {
+                return currentState;
+            }
+
+            let stateStr = currentState.toString();
+            if (visited.has(stateStr)) {
+                continue;
+            }
+            visited.add(stateStr);
+
+            let childStates = currentState.generateChildStates();
+            childStates.forEach(state => {
+                if (!visited.has(state.toString())) {
+                    queue.push(state);
+                }
+            });
+        }
+
+        return null;
+    }
+
+    
+
+    // 可能な子ノード（次の状態）を生成するメソッド
+    generateChildStates() {
+        let children = [];
+        let emptyIndex = this.tiles.indexOf('empty');
+
+        // 空白タイルの移動可能な位置を計算
+        let possibleMoves = this.getPossibleMoves(emptyIndex);
+
+        // 各移動に対して新しい状態を生成
+        possibleMoves.forEach(index => {
+            let newTiles = this.tiles.slice();
+            [newTiles[emptyIndex], newTiles[index]] = [newTiles[index], newTiles[emptyIndex]];
+            let moveDescription = `Move tile at index ${index} to empty space`; // 移動の説明
+            children.push(new PuzzleState(newTiles, this, moveDescription));
+        });
+
+        return children;
+    }
+
+    // 空白タイルの移動可能な位置を取得するメソッド
+    getPossibleMoves(emptyIndex) {
+        let moves = [];
+        let row = Math.floor(emptyIndex / 3);
+        let col = emptyIndex % 3;
+
+        // 上に移動可能か
+        if (row > 0) moves.push(emptyIndex - 3);
+        // 下に移動可能か
+        if (row < 2) moves.push(emptyIndex + 3);
+        // 左に移動可能か
+        if (col > 0) moves.push(emptyIndex - 1);
+        // 右に移動可能か
+        if (col < 2) moves.push(emptyIndex + 1);
+
+        return moves;
+    }
+
+    // 状態を文字列で表現するメソッド（探索済み状態のチェックに使用）
+    toString() {
+        return this.tiles.join(',');
+    }
+}
+
+
 document.addEventListener('DOMContentLoaded', function () {
     const container = document.getElementById('puzzle-container');
     let tiles = ['tile_0_0', 'tile_0_1', 'tile_0_2', 'tile_1_0', 'tile_1_1', 'tile_1_2', 'tile_2_0', 'tile_2_1', 'empty'];
+    let initialState = new PuzzleState(['tile_0_0', 'tile_0_1', 'tile_0_2', 'tile_1_0', 'tile_1_1', 'tile_1_2', 'tile_2_0', 'tile_2_1', 'empty']);
 
     renderTiles()
-    
+
     function renderTiles() {
         container.innerHTML = '';
         tiles.forEach(tile => {
@@ -19,10 +136,210 @@ document.addEventListener('DOMContentLoaded', function () {
         });
     }
 
-    document.getElementById('move-up').addEventListener('click', moveEmptyDown); // 空白を上に動かすため下のタイルを上に移動
-    document.getElementById('move-down').addEventListener('click', moveEmptyUp); // 空白を下に動かすため上のタイルを下に移動
-    document.getElementById('move-left').addEventListener('click', moveEmptyRight); // 空白を左に動かすため右のタイルを左に移動
-    document.getElementById('move-right').addEventListener('click', moveEmptyLeft); // 空白を右に動かすため左のタイルを右に移動
+    // 空白タイルを動かす関数
+    function moveEmpty(tile) {
+        let index = tiles.indexOf(tile);
+        let emptyIndex = tiles.indexOf('empty');
+        if (canMove(index, emptyIndex)) {
+            [tiles[index], tiles[emptyIndex]] = [tiles[emptyIndex], tiles[index]];
+            renderTiles();
+        }
+    }
+
+    // ボタン操作による空白タイルの移動関数
+    // ボタン操作による空白タイルの移動関数
+    function moveEmptyByButton(direction) {
+        let emptyIndex = tiles.indexOf('empty');
+        let tileToMove;
+
+        switch (direction) {
+            case 'up':
+                tileToMove = emptyIndex - 3 >= 0 ? tiles[emptyIndex - 3] : null; // 上に移動するため、空白より上のタイルを選択
+                break;
+            case 'down':
+                tileToMove = emptyIndex + 3 < tiles.length ? tiles[emptyIndex + 3] : null; // 下に移動するため、空白より下のタイルを選択
+                break;
+            case 'left':
+                tileToMove = emptyIndex % 3 > 0 ? tiles[emptyIndex - 1] : null; // 左に移動するため、空白より左のタイルを選択
+                break;
+            case 'right':
+                tileToMove = emptyIndex % 3 < 2 ? tiles[emptyIndex + 1] : null; // 右に移動するため、空白より右のタイルを選択
+                break;
+        }
+
+        if (tileToMove) {
+            moveEmpty(tileToMove);
+        }
+    }
+
+
+    // タイルが動かせるかどうか判定する関数
+    function canMove(index, emptyIndex) {
+        let row = Math.floor(index / 3);
+        let col = index % 3;
+        let emptyRow = Math.floor(emptyIndex / 3);
+        let emptyCol = emptyIndex % 3;
+
+        return (row === emptyRow && Math.abs(col - emptyCol) === 1) || (col === emptyCol && Math.abs(row - emptyRow) === 1);
+    }
+
+    function shuffleTiles() {
+        let shuffleMoves = 100; // シャッフルのための動きの回数
+        let delay = 50; // ミリ秒単位での遅延時間
+
+        for (let i = 0; i < shuffleMoves; i++) {
+            setTimeout(function () {
+                let emptyIndex = tiles.indexOf('empty');
+                let possibleMoves = [];
+
+                // 空白タイルの上にタイルがある場合
+                if (emptyIndex >= 3) possibleMoves.push(emptyIndex - 3);
+                // 空白タイルの下にタイルがある場合
+                if (emptyIndex <= 5) possibleMoves.push(emptyIndex + 3);
+                // 空白タイルの左にタイルがある場合
+                if (emptyIndex % 3 !== 0) possibleMoves.push(emptyIndex - 1);
+                // 空白タイルの右にタイルがある場合
+                if (emptyIndex % 3 !== 2) possibleMoves.push(emptyIndex + 1);
+
+                // ランダムに選ばれた動きを実行
+                let randomMove = possibleMoves[Math.floor(Math.random() * possibleMoves.length)];
+                moveEmpty(tiles[randomMove]);
+            }, i * delay);
+        }
+    }
+
+    let intervalId; // グローバル変数でインターバルIDを保持
+
+    function randomMove() {
+        const delay = 200; // 500ミリ秒ごとに動かす
+        intervalId = setInterval(function () {
+            let emptyIndex = tiles.indexOf('empty');
+            let possibleMoves = [];
+
+            if (emptyIndex >= 3) possibleMoves.push(emptyIndex - 3);
+            if (emptyIndex <= 5) possibleMoves.push(emptyIndex + 3);
+            if (emptyIndex % 3 !== 0) possibleMoves.push(emptyIndex - 1);
+            if (emptyIndex % 3 !== 2) possibleMoves.push(emptyIndex + 1);
+
+            let randomMove = possibleMoves[Math.floor(Math.random() * possibleMoves.length)];
+            moveEmpty(tiles[randomMove]);
+            checkCompletion()
+        }, delay);
+    }
+
+    function stopRandomMove() {
+        clearInterval(intervalId);
+    }
+
+    function checkCompletion() {
+        const correctOrder = ['tile_0_0', 'tile_0_1', 'tile_0_2', 'tile_1_0', 'tile_1_1', 'tile_1_2', 'tile_2_0', 'tile_2_1', 'empty'];
+        if (tiles.join('') === correctOrder.join('')) {
+            alert('パズル完成！');
+        }
+    }
+
+    function displaySolution(goalState) {
+        let currentState = goalState;
+        let steps = [];
+
+        while (currentState.parent) {
+            steps.push(currentState.move);
+            currentState = currentState.parent;
+        }
+
+        steps.reverse(); // 初期状態からゴール状態への順序にする
+        steps.forEach(step => console.log(step));
+    }
+
+    // 解を実行する関数内で applyMove を使用
+    function executeSolution(solution, renderFunction,span) {
+        let stepIndex = 0;
+    
+        function nextStep() {
+            if (stepIndex < solution.length) {
+                applyMove(tiles, solution[stepIndex]);
+                renderFunction(); // パズルを再描画
+    
+                // 現在の手数を表示
+                
+                console.log(`手数: ${stepIndex + 1} / ${solution.length}`);
+                
+                
+                stepIndex++;
+                setTimeout(nextStep, span); // 次のステップまでの遅延
+            }
+        }
+    
+        nextStep();
+    }
+
+    // タイルの状態を更新する関数
+    function applyMove(tiles, move) {
+        // 移動命令からタイルのインデックスを抽出
+        let match = move.match(/index (\d+)/);
+        if (!match) return; // 移動命令が正しくない場合は何もしない
+
+        let tileIndex = parseInt(match[1], 10); // インデックスを数値に変換
+        let emptyIndex = tiles.indexOf('empty');
+
+        // タイルと空白タイルの位置を交換
+        [tiles[emptyIndex], tiles[tileIndex]] = [tiles[tileIndex], tiles[emptyIndex]];
+    }
+
+    //解の手順を取得する関数
+    function getSolutionSteps(goalState) {
+        let currentState = goalState;
+        let steps = [];
+
+        while (currentState.parent) {
+            steps.push(currentState.move);
+            currentState = currentState.parent;
+        }
+
+        return steps.reverse(); // 初期状態からゴール状態への順序
+    }
+
+    
+
+
+
+    // ボタンにイベントリスナーを追加
+    document.getElementById('move-up').addEventListener('click', () => moveEmptyByButton('up'));
+    document.getElementById('move-down').addEventListener('click', () => moveEmptyByButton('down'));
+    document.getElementById('move-left').addEventListener('click', () => moveEmptyByButton('left'));
+    document.getElementById('move-right').addEventListener('click', () => moveEmptyByButton('right'));
 
     document.getElementById('shuffle-button').addEventListener('click', shuffleTiles);
+    document.getElementById('start-random-moves').addEventListener('click', randomMove);
+    document.getElementById('stop-random-moves').addEventListener('click', stopRandomMove);
+    document.getElementById('check-button').addEventListener('click', checkCompletion);
+
+    document.getElementById('dfs-button').addEventListener('click', function () {
+        let initialState = new PuzzleState([...tiles]);
+        let goalState = PuzzleState.dfs(initialState);
+
+        if (goalState) {
+            console.log("ゴールに到達しました！");
+            let solution = getSolutionSteps(goalState);
+            console.log(`解を見つけました。手数: ${solution.length}`);
+            executeSolution(solution, renderTiles,1); // 解を実行する関数を呼び出す
+        } else {
+            console.log("ゴールに到達できませんでした。");
+        }
+    });
+
+    document.getElementById('bfs-button').addEventListener('click', function() {
+        let initialState = new PuzzleState([...tiles]);
+        let goalState = PuzzleState.bfs(initialState);
+    
+        if (goalState) {
+            console.log("ゴールに到達しました！");
+            let solution = getSolutionSteps(goalState);
+            displaySolution(solution);
+            executeSolution(solution, renderTiles,500);
+        } else {
+            console.log("ゴールに到達できませんでした。");
+        }
+    });
+    
 });
